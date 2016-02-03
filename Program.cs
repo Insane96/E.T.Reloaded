40dce9c9e9c1e6a041ebb301334780be421a7e07
Insane-96 <insane96mcp@gmail.com>
2016-02-03 13:15:17 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E.T._Reloaded
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("First Commit");
+			Console.ReadLine();
+		}
+	}
+}
