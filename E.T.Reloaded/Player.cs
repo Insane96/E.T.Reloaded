0e9025f8e56b0ca04cfe1405762a6a161dff8929
DrTonyChopper <remo.orlandini@virgilio.it>
2016-02-15 11:40:31 +0100

    Fixed bug

--- E.T.Reloaded/Player.cs
+++ E.T.Reloaded/Player.cs
@@ -4,17 +4,19 @@ using OpenTK;
 
 namespace E.T.Reloaded
 {
-	public class Player
+	public class Player:Obj2D
 	{
 		public Texture texture;
 		public Sprite sprite;
-		Obj2D obj2D;
-		public Player ()
+
+		public Player (int x = 0, int y = 0):base(x,y,0,0)
 		{
 			texture = new Texture ("../../Assets/RFVF_Rosso_1.png");
 			sprite = new Sprite (texture.Width, texture.Height);
 			sprite.scale = new Vector2 (3f, 3f);
-			obj2D.Position = sprite.position;
+			Position = sprite.position;
+			width = texture.Width;
+			height = texture.Height;
 		}
 	}
 }
